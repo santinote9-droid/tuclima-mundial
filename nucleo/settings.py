474e1d085fe2bf4cf9193e1f372b89993285d21e
Santinote9 <santinote9@gmail.com>
2026-02-01 17:49:32 -0300

    Arreglo login y favicon

--- nucleo/settings.py
+++ nucleo/settings.py
@@ -150,3 +150,5 @@ LOCALE_PATHS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CSRF_TRUSTED_ORIGINS = ['https://tuclima-mundial.onrender.com']
