a7c42ef68e75f6e8402a80c91fbb3d410d4d714e
Santinote9 <santinote9@gmail.com>
2026-02-16 00:03:24 -0300

    Sistema de usuarios permanente activado

--- nucleo/settings.py
+++ nucleo/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'nucleo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:Sangioff_23@db.lmumvstjdyaozewqygfx.supabase.co:5432/postgres'
+    )
 }
 
 
